b3c519b24c9b385f34df3fa1f088dc610f8f578c
Kojiro-schatten <banbirou@icloud.com>
2022-01-27 04:32:13 +0900

    feat: REST API function with oak

--- deno/app.ts
+++ deno/app.ts
@@ -3,6 +3,11 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 import todosRoutes from "./routes/todos.ts"
 const app = new Application();
 
+app.use(async (ctx, next) => {
+  console.log('Middleware!');
+  await next();
+})
+
 app.use(todosRoutes.routes());
 app.use(todosRoutes.allowedMethods());
 
--- deno/routes/todos.ts
+++ deno/routes/todos.ts
@@ -10,19 +10,42 @@ interface Todo {
 let todos: Todo[] = [];
 
 router.get('/todos', (ctx) => {
-  ctx.response.body = { todos: todos }
+  ctx.response.body = { todos: todos };
+});
+
+router.post('/todos', async (ctx) => {
+  const result = await ctx.request.body();
+  // console.log('result');
+  const text = await result.value;
+  // console.log(text.value);
+  const newTodo: Todo = {
+    id: new Date().toISOString(),
+    text: text,
+  };
+  todos.push(newTodo)
+  ctx.response.body = {
+    message: 'Created!',
+    todo: newTodo
+  };
 })
 
-router.post('/todos', (ctx) => {
-
-})
-
-router.put('/todos/:todoId', (ctx) => {
-
+router.put('/todos/:todoId', async (ctx) => {
+  const result = await ctx.request.body();
+  const text = await result.value;
+  const tid = ctx.params.todoId;
+  const todoIndex = todos.findIndex((todo) => {
+    return (todo.id = tid);
+  });
+  todos[todoIndex] = { id: todos[todoIndex].id, text: text };
+  ctx.response.body = {message: 'Updated Todo'}
 })
 
 router.delete('/todos/:todoId', (ctx) => {
-
+  const tid = ctx.params.todoId;
+  todos = todos.filter((todo) => todo.id !==tid)
+  ctx.response.body = {
+    message: 'Deleted todo'
+  }
 })
 
 export default router
