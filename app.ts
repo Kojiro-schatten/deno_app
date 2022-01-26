89a6d3cc31f575de1dffcd62797ec432cd3a5bd4
Kojiro-schatten <banbirou@icloud.com>
2022-01-27 03:44:09 +0900

    add: 3rd party library, oak

--- app.ts
+++ app.ts
@@ -1,3 +1,13 @@
-import { serve } from "https://deno.land/std@0.122.0/http/server.ts";
+// import { serve } from "https://deno.land/std@0.122.0/http/server.ts";
 
-serve(() => new Response("Hello World\n"));
+// serve(() => new Response("Hello World\n"));
+
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = "Hello World from oak!";
+});
+
+await app.listen({ port: 8000 });
